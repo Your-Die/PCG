dd2fb0e8932a352bad37bd17fde42b36944946ff
Jordy <jordy_van_opstal@live.nl>
2019-12-28 16:53:11 +0100

    Implement SmotohNoiseGenerator.

---
+++ Assets/Scripts/Generation/Grid/Scripts/2D/SmoothNoiseGenerator.cs
@@ -0,0 +1,58 @@
+using Chinchillada.Utilities;
+using UnityEngine;
+
+namespace Chinchillada.Generation.Grid
+{
+    public class SmoothNoiseGenerator : IGenerator<Grid2D>
+    {
+        private readonly int samplePeriod;
+
+        private readonly IGenerator<Grid2D> gridGenerator;
+
+        public SmoothNoiseGenerator(IGenerator<Grid2D> gridGenerator, int samplePeriod)
+        {
+            this.gridGenerator = gridGenerator;
+            this.samplePeriod = samplePeriod;
+        }
+        
+        public Grid2D Generate()
+        {
+            var grid = this.gridGenerator.Generate();
+            return SmoothNoise(grid, this.samplePeriod);
+        }
+
+        public static Grid2D SmoothNoise(Grid2D grid, int samplePeriod)
+        {
+            var sampleFrequency = 1.0f / samplePeriod;
+            var output = grid.CopyShape();
+
+            for (var x = 0; x < output.Width; x++)
+            {
+                var leftBorder = x.ClosestSmallerMultiple(samplePeriod);
+                var rightBorder = (leftBorder + samplePeriod) % output.Width;
+
+                var horizontalBlend = (x - leftBorder) * sampleFrequency;
+
+                for (var y = 0; y < output.Height; y++)
+                {
+                    var topBorder = y.ClosestSmallerMultiple(samplePeriod);
+                    var bottomBorder = (topBorder + samplePeriod) % output.Height;
+
+                    var verticalBlend = (y - topBorder) * sampleFrequency;
+
+                    var topLeft = grid[leftBorder, topBorder];
+                    var topRight = grid[rightBorder, topBorder];
+                    var bottomLeft = grid[leftBorder, bottomBorder];
+                    var bottomRight = grid[rightBorder, bottomBorder];
+
+                    var top = Mathf.Lerp(topLeft, topRight, horizontalBlend);
+                    var bottom = Mathf.Lerp(bottomLeft, bottomRight, horizontalBlend);
+
+                    output[x, y] = (int)Mathf.Lerp(top, bottom, verticalBlend);
+                }
+            }
+
+            return output;
+        }
+    }
+}
