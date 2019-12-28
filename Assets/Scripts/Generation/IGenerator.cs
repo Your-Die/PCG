329dcabd9c7600e6b9e72c9a17fb4e0686d0ce62
Jordy <jordy_van_opstal@live.nl>
2019-12-28 16:50:43 +0100

    Add observable generator.

--- Assets/Scripts/Generation/IGenerator.cs
+++ Assets/Scripts/Generation/IGenerator.cs
@@ -1,7 +1,24 @@
+using System;
+using System.Collections.Generic;
+
 namespace Chinchillada.Generation
 {
     public interface IGenerator<out T>
     {
         T Generate();
+    }
+
+    public interface IObservableGenerator<out T> : IGenerator<T>
+    {
+        event Action<T> Generated;
+    }
+
+    public static class GeneratorExtensions
+    {
+        public static IEnumerable<T> Generate<T>(this IGenerator<T> generator, int amount)
+        {
+            for (var i = 0; i < amount; i++)
+                yield return generator.Generate();
+        }
     }
 }
