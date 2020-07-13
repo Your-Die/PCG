20c393137adfbf78cf38cb003c6300d69d4aec89
Jordy <jordy_van_opstal@live.nl>
2020-07-14 00:42:37 +0200

    Implement base classes for non-iterative generators

---
+++ Scripts/GeneratorBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Chinchillada.Generation
+{
+    public abstract class GeneratorBase<T> : IGenerator<T>
+    {
+        public T Result { get; private set; }
+
+        public event Action<T> Generated;
+
+        public T Generate()
+        {
+            var result = this.GenerateInternal();
+            
+            this.RegisterResult(result);
+         
+            return this.Result;
+        }
+
+        protected abstract T GenerateInternal();
+
+        protected void RegisterResult(T result)
+        {
+            this.Result = result;
+            this.Generated?.Invoke(this.Result);
+        }
+    }
+}
---
+++ Scripts/GeneratorComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Chinchillada.Foundation;
+using UnityEngine;
+
+namespace Chinchillada.Generation
+{
+    public class GeneratorComponent<T> : ChinchilladaBehaviour, IGenerator<T>
+    {
+        [SerializeField] private IGenerator<T> generator;
+        public T Result => this.generator.Result;
+
+        public event Action<T> Generated
+        {
+            add => this.generator.Generated += value;
+            remove => this.generator.Generated -= value;
+        }
+
+        public T Generate() => this.generator.Generate();
+    }
+}
