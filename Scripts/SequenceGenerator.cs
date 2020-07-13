1a581bbdab7eb417ae2169a8f4b7d7955a84791f
Jordy <jordy_van_opstal@live.nl>
2020-07-14 00:43:22 +0200

    Create sequence Generator

---
+++ Scripts/SequenceGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chinchillada.Generation
+{
+    [Serializable]
+    public class SequenceGenerator<T> : GeneratorBase<IList<T>>
+    {
+        [SerializeField] private IGenerator<T> itemGenerator;
+        
+        [SerializeField] private int length;
+        
+        protected override IList<T> GenerateInternal()
+        {
+            var sequence = new T[this.length];
+            
+            for (var i = 0; i < this.length; i++) 
+                sequence[i] = this.itemGenerator.Generate();
+
+            return sequence;
+        }
+    }
+}
