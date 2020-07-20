c61e254a9d0e549248de39fe87a8b0e0ab199348
Jordy <jordy_van_opstal@live.nl>
2020-07-20 13:36:33 +0200

    Add RandomColorGraphic.

---
+++ Scripts/Colorscheme/RandomColorGraphic.cs
@@ -0,0 +1,27 @@
+﻿using Chinchillada.Foundation;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Chinchillada.Colors
+{
+    public class RandomColorGraphic : ChinchilladaBehaviour
+    {
+        [SerializeField] private IColorScheme colorScheme;
+
+        [SerializeField, FindComponent] private Graphic graphic;
+
+        [SerializeField] private bool applyOnAwake;
+
+        [Button]
+        public void Apply() => this.graphic.color = this.colorScheme.ChooseRandom();
+
+        protected override void Awake()
+        {
+            base.Awake();
+            
+            if (this.applyOnAwake) 
+                this.Apply();
+        }
+    }
+}
