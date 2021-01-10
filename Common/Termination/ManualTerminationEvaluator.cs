350462f3b5706768383f83c8a7d29342bd9af147
Jordy <jordy_van_opstal@live.nl>
2021-01-10 01:16:31 +0100

    Add ManualTerminationEvaluator

---
+++ Common/Termination/ManualTerminationEvaluator.cs
@@ -0,0 +1,23 @@
+﻿namespace Chinchillada.Generation
+{
+    using System;
+    using UnityEngine;
+
+    [Serializable]
+    public class ManualTerminationEvaluator : ITerminationEvaluator
+    {
+        [SerializeField] private bool isFinished;
+
+        public bool IsFinished
+        {
+            get => this.isFinished;
+            set => this.isFinished = value;
+        }
+
+        public void SetFinished() => this.isFinished = true;
+
+        public void Reset() => this.IsFinished = false;
+
+        public bool Evaluate() => this.IsFinished;
+    }
+}
